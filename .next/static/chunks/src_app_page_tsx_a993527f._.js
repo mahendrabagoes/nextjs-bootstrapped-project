022e9068966bbcc3982f03e42a7ee76c0af3085f
mahendrabagoes <34390131+mahendrabagoes@users.noreply.github.com>
2025-05-12 18:24:36 +0700

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747049075435

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ba3fa545._.js",
+  "static/chunks/node_modules_c998cb3a._.js"
+],
+    source: "dynamic"
+});
